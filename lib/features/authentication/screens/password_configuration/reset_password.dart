6e37a956c5d27eda453fabf11e7185a389ea9c52
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-04-18 12:06:24 +0530

    Screens

--- lib/features/authentication/screens/password_configuration/reset_password.dart
+++ lib/features/authentication/screens/password_configuration/reset_password.dart
@@ -1,54 +1,34 @@
+import 'package:app/features/authentication/screens/password_configuration/reset_password.dart';
 import 'package:app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../../../utils/constants/text_strings.dart';
 
-class ForgetPassword extends StatelessWidget {
-  const ForgetPassword({super.key});
+class ResetPassword extends StatelessWidget {
+  const ResetPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(TSizes.defaultSpace),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      body: const SingleChildScrollView(
+        child: Padding(
+         padding:  EdgeInsets.all(TSizes.defaultSpace),
+         child: Column(
           children: [
-            // Headings
-            Text(
-              TTexts.forgetPasswordTitle,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const SizedBox(height: TSizes.spaceBtwItems),
-            Text(
-              TTexts.forgetPasswordSubTitle,
-              style: Theme.of(context).textTheme.labelMedium,
-            ),
-            const SizedBox(height: TSizes.spaceBtwSections * 2),
 
-            // Text Field
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: TTexts.email,
-                prefixIcon: Icon(Iconsax.direct_right),
-              ),
-            ),
+            //image
 
-            const SizedBox(height: TSizes.spaceBtwSections),
+            //title
 
-            /// Submit Button
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text(TTexts.submit),
-              ),
-            ),
+            //subtitle
           ],
         ),
       ),
+      )
     );
   }
 }
