fa16aaed7ffe5b07f52133d138d36a7a811124c4
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-05-03 13:25:44 +0530

    Errors

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -13,25 +13,40 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             ClipPath(
-              clipper: TCustomCurvedEdges(),
-              child: Container(
-                color: TColors.primary,
-                padding: const EdgeInsets.all(0),
-                child: SizedBox(
-                  height: 400,
-                  child: Stack(
-                    children: [
-                      Positioned(top: -150, right:-250, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
-                      Positioned(top: 100, right:-300, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
+              clipper: TCustomCurvedEdges(
+                  child: Container(
+                    color: TColors.primary,
+                    padding: const EdgeInsets.all(0),
+                    child: SizedBox(
+                      height: 400,
+                      child: Stack(
+                        children: [
+                          Positioned(top: -150, right:-250, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
+                          Positioned(top: 100, right:-300, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
 
-                    ],
-                  ),
-                ),
+                        ],
+                      ),
+                    ),
+                  );
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class TCurvedEdgeWidget extends StatelessWidget {
+  const TCurvedEdgeWidget({
+    super.key, this.child,
+  });
+  final Widget? child;
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: TCustomCurvedEdges(),
+      child: child,
     );
   }
 }
