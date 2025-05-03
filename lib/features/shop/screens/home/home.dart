461932fec134eaf6b7d4e91a606fc7ebf597df98
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-05-03 12:48:57 +0530

    Shaping

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
 import '../../../../utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,51 +11,26 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              color: TColors.primary,
-              padding: const EdgeInsets.all(0),
-              child: Stack(
-                children: [
-                  TCircularContainer(),
-                ],
+            ClipPath(
+              clipper: ,
+              child: Container(
+                color: TColors.primary,
+                padding: const EdgeInsets.all(0),
+                child: SizedBox(
+                  height: 400,
+                  child: Stack(
+                    children: [
+                      Positioned(top: -150, right:-250, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
+                      Positioned(top: 100, right:-300, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
+
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-class TCircularContainer extends StatelessWidget {
-  const TCircularContainer({
-    super.key,
-    this.width,
-    this.height,
-    this.radius =  400,
-    this.padding = 400,
-    this.child,
-    this.backgroundColor = TColors.white,
-  });
-
-  final double? width;
-  final double? height;
-  final double radius;
-  final double padding;
-  final Widget? child;
-  final Color backgroundColor;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 400,
-      height: 480,
-      padding: const EdgeInsets.all(0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(400),
-        color: TColors.textWhite.withOpacity(0.1),
-      ),
-      child: child,
     );
   }
 }
