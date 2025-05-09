43dd7e9f8a58fa9eeaa995bb7e08e8cefc4afcfd
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-05-09 10:45:14 +0530

    Error Fixed IN HOME.DART

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -13,21 +13,31 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             ClipPath(
-              clipper: TCustomCurvedEdges(
-                  child: Container(
-                    color: TColors.primary,
-                    padding: const EdgeInsets.all(0),
-                    child: SizedBox(
-                      height: 400,
-                      child: Stack(
-                        children: [
-                          Positioned(top: -150, right:-250, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
-                          Positioned(top: 100, right:-300, child: TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1))),
-
-                        ],
+              clipper: TCustomCurvedEdges(),
+              child: Container(
+                color: TColors.primary,
+                padding: const EdgeInsets.all(0),
+                child: SizedBox(
+                  height: 400,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: -150,
+                        right: -250,
+                        child: TCircularContainer(
+                          backgroundColor: TColors.textWhite.withOpacity(0.1),
+                        ),
                       ),
-                    ),
-                  );
+                      Positioned(
+                        top: 100,
+                        right: -300,
+                        child: TCircularContainer(
+                          backgroundColor: TColors.textWhite.withOpacity(0.1),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
@@ -39,9 +49,12 @@ class HomeScreen extends StatelessWidget {
 
 class TCurvedEdgeWidget extends StatelessWidget {
   const TCurvedEdgeWidget({
-    super.key, this.child,
+    super.key,
+    this.child,
   });
+
   final Widget? child;
+
   @override
   Widget build(BuildContext context) {
     return ClipPath(
