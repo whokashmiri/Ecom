22d9b70a584894a3ebed3cb0fafbd5e4874ca1ac
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-05-09 11:08:55 +0530

    Primary Design Completed

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,7 +1,5 @@
-import 'package:app/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import 'package:flutter/material.dart';
-import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
-import '../../../../utils/constants/colors.dart';
+import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,56 +8,8 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ClipPath(
-              clipper: TCustomCurvedEdges(),
-              child: Container(
-                color: TColors.primary,
-                padding: const EdgeInsets.all(0),
-                child: SizedBox(
-                  height: 400,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        top: -150,
-                        right: -250,
-                        child: TCircularContainer(
-                          backgroundColor: TColors.textWhite.withOpacity(0.1),
-                        ),
-                      ),
-                      Positioned(
-                        top: 100,
-                        right: -300,
-                        child: TCircularContainer(
-                          backgroundColor: TColors.textWhite.withOpacity(0.1),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+        child: Column(children: [TPrimaryHeaderContainer(child: Container())]),
       ),
-    );
-  }
-}
-
-class TCurvedEdgeWidget extends StatelessWidget {
-  const TCurvedEdgeWidget({
-    super.key,
-    this.child,
-  });
-
-  final Widget? child;
-
-  @override
-  Widget build(BuildContext context) {
-    return ClipPath(
-      clipper: TCustomCurvedEdges(),
-      child: child,
     );
   }
 }
