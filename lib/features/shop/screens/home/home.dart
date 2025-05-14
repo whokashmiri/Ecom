05b3071b1cf4262ac48de2fc7b07309984acfeeb
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-05-14 10:35:01 +0530

    Iconx Error

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -5,7 +5,7 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext ) {
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(children: [TPrimaryHeaderContainer(child: Container())]),
